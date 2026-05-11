369277e2cdc75b3321ddd14ff96d0f85d181c7c9
asepjamaludinn <asepjamaludinn24@gmail.com>
2026-05-11 23:09:00 +0700

    refactor: remove unused laravel default files and clean up User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password', 'role', 'leave_quota', 'provider', 'provider_id'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable; 
+    use HasApiTokens;
 
     /**
      * Get the attributes that should be cast.
